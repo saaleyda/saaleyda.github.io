70db1e2375e44ee8b84deab16622037ac3b00bc0
saaleyda <saaaleyd@gmail.com>
2024-09-26 20:10:58 +0700

    Deploy Nuxt.js to gh-pages

---
+++ index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Welcome to My Nuxt.js App</h1>
+      <p>This is the home page created using a .vue file.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #3498db;
+  }
+  </style>
+  
